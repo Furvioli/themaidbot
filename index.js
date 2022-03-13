6b57d87230072e15a0100f0d1cb0af13a35404b5
Furvioli <gabriele.intonazzo.2005@gmail.com>
2022-03-13 13:19:07 +0100

    initialite github repository

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS",
     ]
 })
 
@@ -20,5 +23,14 @@ client.on("messageCreate", (message) => {
 
 })
 
+const welcomeChannelId = "952504192507973662"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Benvenuto nel server, abbia una piacevole permanenza! uwu`,
+        files: [img]
+    })
+})
 
 client.login(process.env.TOKEN)
